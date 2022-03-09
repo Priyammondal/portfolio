4587a97a7dbc2938d6eba583bd03097752cfed7c
Bdiplus <priyam.mondal@bdiplus.com>
2022-03-10 00:36:03 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 // import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import NavBar from "./components/NavBar.jsx"
 
 function App() {
   return (
-    <div className="App">
-     Hi, Priyam Mondal...
+    <div>
+      <NavBar/>
     </div>
   );
 }
--- src/priyam.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const priyam = () => {
-  return (
-    <div>priyam</div>
-  )
-}
