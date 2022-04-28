caa88ae63073b9793568dfb4fe4808e3cbcacb22
Bdiplus <priyam.mondal@bdiplus.com>
2022-04-29 01:29:49 +0530

    push

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import logo from "../components/Images/code.png";
 import logo from "../components/Images/manLogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -27,27 +27,108 @@ const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav" style={{ marginLeft: "auto" }}>
               <li class="nav-item">
-                <a class="nav-link" href="#home">
+                <a
+                  id="homee"
+                  class="nav-link"
+                  href="#home"
+                  style={{}}
+                  onClick={() => {
+                    // let url = window.location.href.slice(
+                    //   window.location.href.indexOf("#"),
+                    //   window.location.href.length
+                    // );
+                    // console.log(url)
+                    // if (url == "#home") {
+                      let element = document.getElementById("homee");
+                      element.style.borderBottom = "0.1875rem solid #f9ab00";
+                    // }
+                  }}
+                >
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#aboutMe">
+                <a
+                  id="aboutMee"
+                  className="nav-link"
+                  href="#aboutMe"
+                  style={{}}
+                  onClick={() => {
+                    // let url = window.location.href.slice(
+                    //   window.location.href.indexOf("#"),
+                    //   window.location.href.length
+                    // );
+                    // console.log(url)
+                    // if (url == "#aboutMe") {
+                      let element = document.getElementById("aboutMee");
+                      element.style.borderBottom = "0.1875rem solid #f9ab00";
+                    // }
+                  }}
+                >
                   about me
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#services">
+                <a
+                  id="service"
+                  className="nav-link"
+                  href="#services"
+                  style={{}}
+                  onClick={() => {
+                    // let url = window.location.href.slice(
+                    //   window.location.href.indexOf("#"),
+                    //   window.location.href.length
+                    // );
+                    // console.log(url)
+                    // if (url == "#services") {
+                      let element = document.getElementById("service");
+                      element.style.borderBottom = "0.1875rem solid #f9ab00";
+                    // }
+                  }}
+                >
                   services
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#projects">
+                <a
+                  id="project"
+                  className="nav-link"
+                  aria-current="page"
+                  href="#projects"
+                  style={{}}
+                  onClick={() => {
+                    // let url = window.location.href.slice(
+                    //   window.location.href.indexOf("#"),
+                    //   window.location.href.length
+                    // );
+                    // console.log(url)
+                    // if (url == "#projects") {
+                      let element = document.getElementById("project");
+                      element.style.borderBottom = "0.1875rem solid #f9ab00";
+                    // }
+                  }}
+                >
                   Projects
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#contact">
+                <a
+                  id="contacts"
+                  className="nav-link"
+                  href="#contact"
+                  style={{}}
+                  onClick={() => {
+                    // let url = window.location.href.slice(
+                    //   window.location.href.indexOf("#"),
+                    //   window.location.href.length
+                    // );
+                    // console.log(url)
+                    // if (url == "#contact") {
+                      let element = document.getElementById("contacts");
+                      element.style.borderBottom = "0.1875rem solid #f9ab00";
+                    // }
+                  }}
+                >
                   contacts
                 </a>
               </li>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,170 @@
-import React from 'react'
+import React from "react";
+import bdiplus from "./Images/bdiplusLogo.png";
+import cakeShop from "./Images/cakeShop.png";
+import calculator from "./Images/calculator.png";
+import crud from "./Images/crud.png";
+import travelingWebsite from "./Images/travelingWebsite.png";
+import registration from "./Images/registration.png";
+import laptopBazar from "./Images/laptopBazar.png";
+import portfolio from "./Images/portfolio.png";
 
 const Projects = () => {
   return (
-    <div id='projects'>Projects</div>
-  )
-}
+    <div id="projects">
+      <div
+      className="projects"
+        style={{
+          height: "30%",
+          width: "90%",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <a
+          href="https://signup.onedata.plus/"
+          target="_blank"
+          style={{ height: "100%", width: "22%" }}
+        >
+          <img
+            src={bdiplus}
+            alt="bdiplus"
+            style={{
+              height: "100%",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "50px",
+            }}
+          />
+        </a>
 
-export default Projects
+        <a
+          href="https://github.com/Priyammondal/Cakeshop"
+          target="_blank"
+          style={{ height: "100%", width: "22%" }}
+        >
+          <img
+            src={cakeShop}
+            alt="cakeShop"
+            style={{
+              height: "100%",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "50px",
+            }}
+          />
+        </a>
+        <a
+          href="https://github.com/Priyammondal/Travelling-website"
+          target="_blank"
+          style={{ height: "100%", width: "22%" }}
+        >
+          <img
+            src={travelingWebsite}
+            alt="travelingWebsite"
+            style={{
+              height: "100%",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "50px",
+            }}
+          />
+        </a>
+        <a
+          href="https://github.com/Priyammondal/Registration-Form-React"
+          target="_blank"
+          style={{ height: "100%", width: "22%" }}
+        >
+          <img
+            src={registration}
+            alt="registration"
+            style={{
+              height: "100%",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "50px",
+              borderRadius: "50px",
+            }}
+          />
+        </a>
+      </div>
+
+      <div
+      className="projects"
+        style={{
+          height: "30%",
+          width: "90%",
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "5%",
+        }}
+      >
+        <a
+          href="https://github.com/ND-Geek/Calculator"
+          target="_blank"
+          style={{ height: "100%", width: "22%" }}
+        >
+          <img
+            src={calculator}
+            alt="calculator"
+            style={{
+              height: "100%",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "50px",
+            }}
+          />
+        </a>
+        <a
+          href="https://github.com/Priyammondal/random-1"
+          target="_blank"
+          style={{ height: "100%", width: "22%" }}
+        >
+          <img
+            src={crud}
+            alt="crud"
+            style={{
+              height: "100%",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "50px",
+            }}
+          />
+        </a>
+        <a
+          href="https://github.com/Priyammondal/laptopBazar"
+          target="_blank"
+          style={{ height: "100%", width: "22%" }}
+        >
+          <img
+            src={laptopBazar}
+            alt="laptopBazar"
+            style={{
+              height: "100%",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "50px",
+            }}
+          />
+        </a>
+        <a
+          href="https://github.com/Priyammondal/portfolio"
+          target="_blank"
+          style={{ height: "100%", width: "22%" }}
+        >
+          <img
+            src={portfolio}
+            alt="portfolio"
+            style={{
+              height: "100%",
+              width: "100%",
+              cursor: "pointer",
+              borderRadius: "50px",
+            }}
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
